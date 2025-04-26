a0c30478efa4e2a78486fe82de28789e7f6d957d
Sudheendra <sudheendra579@gmail.com>
2025-04-26 11:45:20 +0530

    made a few change sto fix port isuue and created a yaml file

--- backend/app.py
+++ backend/app.py
@@ -97,4 +97,5 @@ def check_guess():
 
 if __name__ == '__main__':
      from waitress import serve
-     serve(app, host="0.0.0.0", port=8080)
+     port = int(os.environ.get("PORT", 8080))
+     serve(app, host="0.0.0.0", port=port)
